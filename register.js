328e11545a871d98a21ee7818429d552efe50302
majidsaadati <majidsaadati021@gmail.com>
2023-09-29 23:09:04 +0330

    add js and add coments and final edit on html and css

---
+++ register.js
@@ -0,0 +1,65 @@
+// سلکت کردن متغییر ها
+const contactName = document.querySelector('#userName')
+const numberPhone = document.querySelector('#numberPhone')
+const button = document.querySelector('#button')
+const form = document.querySelector('form')
+// جلوگیری از ریفرش شدن صفحه 
+form.addEventListener('submit', e =>{
+    e.preventDefault()
+})
+//ساخت تابع ارور
+const error = ( input, massage) => {
+    let p = document.querySelector('p')
+    p.innerText = massage
+    p.classList.add('text-red-500')
+    input.classList.add('border-2 border-rose-500')
+}
+// ساخت تابع صحیح بودن ولیدیشن
+const oksub = (massage) => {
+    let p = document.querySelector('p')
+    p.innerText = massage
+    p.classList.add('text-green-600')
+  
+
+}
+
+// انجام ولیدیشن با کلبک بر روی دکمه
+button.addEventListener('click', validation)
+
+// تابع ولیدیشن نام کاربری و شماره تلفن
+function validation() {
+// گرفتن فضای خالی از مقدار نام کار بری و شماره تلفن
+    const contactNameValue = contactName.value.trim()
+    const numberPhoneValue = numberPhone.value.trim()
+    
+    if(contactNameValue === '') {
+        error(contactName, 'نام کاربری را وارد نمایید')
+        contactName.classList.add('border-red-600')
+        return false
+    }else
+    if(contactNameValue.length < 4) {
+        error(contactName, 'نام کاربری باید بیشتر از 4 حرف باشد')
+        return false
+    }
+
+    if(numberPhoneValue === '') {
+        error(numberPhone,'شماره تلفن را وارد نمایید')
+        return false
+    }else
+        if(numberPhoneValue.length >= 12){
+        error(numberPhone,'شماره تلفن باید 11 شماره باشد')
+        return false
+    }else
+        if(numberPhoneValue.length <= 10){
+            error(numberPhone,'شماره تلفن باید 11 شماره باشد')
+            return false
+        }else
+        if(numberPhoneValue[0] != 0 && numberPhoneValue[1] != 9){
+            error(numberPhone,'شماره تلفن باید با 09 شروع شود')
+            return false
+        }
+        // انجام تابع با موفقیت
+        oksub('اطلاعات با موفقیت ثبت شد')
+        
+
+    }
